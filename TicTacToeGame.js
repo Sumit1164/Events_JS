55f7264522569e5edc371d0c0c2412d890126eb2
Sumit tripathi <sumittripathi8795@gmail.in>
2025-08-29 23:53:11 +0530

    adding events such as click and so on

--- TicTacToeGame.js
+++ TicTacToeGame.js
@@ -11,3 +11,32 @@ const winPatterns = [
     [0, 4, 8],
     [2, 4, 6]
 ];
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("Box Clicked");
+        if (turn) {
+            box.innerText = "X";
+            turn = false;
+        } else {
+            box.innerText = "O";
+            turn = true;
+        }
+        box.disabled = true;
+        checkWinner();
+    });
+});
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+
+        if (pos1 != "" && pos2 != "" && pos3 != "") {
+            if (pos1 === pos2 && pos2 === pos3) {
+                console.log(`${pos1} is the winner`);
+            }
+        }
+    }
+}
